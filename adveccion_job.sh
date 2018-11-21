b27ff35379c8ada3823eab2c0498a282a5dd2e54
sebastianpb <33877904+sebastianpb@users.noreply.github.com>
2018-11-21 06:51:33 -0500

    Add files via upload

---
+++ adveccion_job.sh
@@ -0,0 +1,8 @@
+#PBS -l nodes=1:ppn=4,mem=1gb,walltime=00:10:00
+#PBS -m abe
+#PBS -N ejercicio29
+
+
+module load anaconda/python3
+cd $PBS_O_WORKDIR # este es el directorio desde donde se ejecuto qsub
+cc adveccion.c -o adveccion
